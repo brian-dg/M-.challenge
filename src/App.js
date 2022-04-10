f076a6e49aa6f1f8184ba8ddef3ca84ff389592f
brian-dg <brian_rc6@hotmail.com>
2022-04-10 16:57:17 -0300

    realizando router

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import { Container } from 'react-bootstrap';
  import Formulario from './components/Formulario';
  import ListUsers from './components/ListUsers'
  import { UsersProvider } from './context/UsersProvider';
- import { BrowserRouter, Route, Switch } from "react-router-dom";
+ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <UsersProvider>
-    <header className='py-5'>
-      <h1>Buscador de Usuarios</h1>
-    </header>
-
-    <Container>
-    
-    <Formulario/>
-    <ListUsers/>
-    </Container>
-    </UsersProvider>
+    <Router>
+      <UsersProvider>
+        <Routes>
+        <Route path="/usuarios" element={<Formulario />}/>
+        
+        </Routes>
+        
+       {/**<Container>     
+          <Formulario/>
+          <ListUsers/>
+        </Container> */} 
+      </UsersProvider>
+    </Router>
   );
 }
 
